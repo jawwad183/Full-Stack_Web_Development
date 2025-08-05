a76b6ccf286183a8aaf982e763885712f67c3fc5
jawwad183 <119884422+jawwad183@users.noreply.github.com>
2025-08-06 02:31:11 +0300

    React

---
+++ React/First-React-App/src/App.jsx
@@ -0,0 +1,13 @@
+import Header,{Fruit,name} from "./Components/Header"
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Fruit/>
+      <h1>{name}</h1>
+    </>
+  )
+}
+
+export default App
---
+++ React/First-React-App/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+function Header(){
+    return(
+        <h1>Hello! word</h1>
+    )
+}
+
+export function Fruit(){
+    return(
+        <h1>Apple</h1>
+    )
+}
+
+export default Header;
+
+export const name = "Jawad"
